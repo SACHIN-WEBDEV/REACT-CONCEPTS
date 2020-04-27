7b1985305361e1c82fec96d5c9692fc137ff5f00
SACHIN-WEBDEV <sachinkumar.webdev@gmail.com>
2020-04-28 01:51:46 +0530

    Updates

---
+++ precache-manifest.cc48e160ca46a17f787cdd48c47bf257.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017022e32b1941158796eb04df6e4769",
+    "url": "/SACHIN-WEBDEV/REACT-CONCEPTS.git/index.html"
+  },
+  {
+    "revision": "c3806075cb69dca6c105",
+    "url": "/SACHIN-WEBDEV/REACT-CONCEPTS.git/static/css/2.33f426b8.chunk.css"
+  },
+  {
+    "revision": "056a4abd8c3818533ad6",
+    "url": "/SACHIN-WEBDEV/REACT-CONCEPTS.git/static/css/main.346108ef.chunk.css"
+  },
+  {
+    "revision": "c3806075cb69dca6c105",
+    "url": "/SACHIN-WEBDEV/REACT-CONCEPTS.git/static/js/2.4802b17b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/SACHIN-WEBDEV/REACT-CONCEPTS.git/static/js/2.4802b17b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056a4abd8c3818533ad6",
+    "url": "/SACHIN-WEBDEV/REACT-CONCEPTS.git/static/js/main.d3cfc951.chunk.js"
+  },
+  {
+    "revision": "aa2ed8a124ac97f35fa9",
+    "url": "/SACHIN-WEBDEV/REACT-CONCEPTS.git/static/js/runtime-main.728b6c09.js"
+  }
+]);
